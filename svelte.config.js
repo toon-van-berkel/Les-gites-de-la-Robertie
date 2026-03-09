e824db369e48161150ad239243988787df30fb6a
Toon-van-Berkel <toonvanberkel203@gmail.com>
2026-03-09 20:45:20 +0100

    update configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
+const hasCustomDomain = process.env.CUSTOM_DOMAIN === 'true';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({ fallback: '404.html' }),
+		paths: {
+			base: dev ? '' : hasCustomDomain ? '' : (process.env.BASE_PATH ?? '')
+		}
 	}
 };
 
