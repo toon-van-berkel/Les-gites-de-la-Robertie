2dd50050881ff5c10fe97643a86662fc2e2dd132
Toon-van-Berkel <toonvanberkel203@gmail.com>
2026-03-09 20:43:17 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
+WORKED
 <!-- <!DOCTYPE html>
 <html lang="en">
 <head>
